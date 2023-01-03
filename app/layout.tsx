2bff44e5acd98721273d0a7e19fe2ecf7a1ffebc
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-01-03 09:24:29 -0600

    feat: eslint, prettier, husky, commitizen, path, and tailwind setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import './globals.css'
+import '@/styles/globals.css';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1"
+        />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
